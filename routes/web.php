76d5e63c783fcdb21e5b8060c04b8e59a8e01e34
bharishh <harish_01@icloud.com>
2025-06-02 22:41:32 +0200

    midleware

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware('guest')->group(function () {
     Route::get('/auth/login', [AuthController::class,'showLogin'])->name('login');
     Route::post('/auth/login',[AuthController::class,'doLogin'])->name('auth.doLogin');
 
+});
 
+Route::middleware('auth')->group(function () {
     Route::get('/auth/logout', [AuthController::class,'logout'])->name('auth.logout');
 
     Route::get('/home/acceuil', function () {return view('home.acceuil');})->name('home.acceuil');
+
+});
